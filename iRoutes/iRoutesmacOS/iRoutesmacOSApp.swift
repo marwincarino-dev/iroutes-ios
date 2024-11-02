c5a3279277e48ce189ebbb7d327938c3d76d03b2
Marwin Carino <mcarino.dev@gmail.com>
2024-11-02 12:50:15 +0800

    Setup project with iOS and macOS apps (with unit and UI tests)

---
+++ iRoutes/iRoutesmacOS/iRoutesmacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRoutesmacOSApp.swift
+//  iRoutesmacOS
+//
+//  Created by Marwin Cariño on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct iRoutesmacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
